a7c1b566863a33bb93e309f1986d4b8e1affa51a
praveenapenumadi <96742808+praveenapenumadi@users.noreply.github.com>
2024-03-19 09:58:48 +0530

    Updated

--- Application/src/main/java/com/example/demo/Application.java
+++ Application/src/main/java/com/example/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-//main method
+//main method to run the spring application
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
